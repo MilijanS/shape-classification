3cf743a2aa2d951081399bf90a9a58cdb7ee0078
Milijan Stojnic <stojnic.milijan@gmail.com>
2017-01-26 09:31:36 +0100

    Update GUI_shape_classification.py

--- GUI_shape_classification.py
+++ GUI_shape_classification.py
@@ -33,6 +33,9 @@ class PaintBox(Frame):
         self.text_entry = Entry(self.myCanvas)
         self.text_entry.pack(side=TOP)
         
+        self.old_x = None
+        self.old_y = None
+        
         button2 = Button(self.myCanvas, text='Learn shape name from single drawn shape')
         button2.bind('<Button-1>', self.save_shapes_for_learning)
         button2.pack(side=TOP) 
@@ -51,6 +54,7 @@ class PaintBox(Frame):
         
         #event handler for drawing
         self.myCanvas.bind( "<B1-Motion>", self.paint)
+        self.myCanvas.bind('<ButtonRelease-1>', self.reset)
          
         #init the classifier module
         shape_classification.import_source_images()    
@@ -99,7 +103,7 @@ class PaintBox(Frame):
     def clear_canvas(self, event):
         self.draw.ellipse((-300, -300, 1200, 1200), fill=white)
         self.myCanvas.create_oval(-300, -300, 1200, 1200, fill='SlateGray1')
-        shape_classification.import_test_images()
+        #shape_classification.import_test_images()
        
     def predict_wrapper(self, event, folder_path=r'test image', filename=r'canvas.jpg'):
         global shapes_learning_pending
@@ -128,14 +132,28 @@ class PaintBox(Frame):
         
     def import_test_image_wrapper(self, event):
         shape_classification.import_test_images()
+        
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
        
     def paint(self, event):
+       
        ofs = 5
        x1, y1 = (event.x - ofs ), (event.y - ofs )
        x2, y2 = (event.x + ofs ), (event.y + ofs )
-       self.myCanvas.create_oval( x1, y1, x2, y2, fill='black')
+       
+       #self.myCanvas.create_oval( x1, y1, x2, y2, fill='black')
+       
        self.draw.ellipse((x1, y1, x2, y2), fill='black')
        
+       if self.old_x and self.old_y:
+            self.myCanvas.create_line(self.old_x, self.old_y, event.x, event.y, width=8, capstyle=ROUND, smooth=TRUE, splinesteps=36)
+            self.draw.line((self.old_x, self.old_y, event.x, event.y), fill=(0,0,0,255), width=8)
+            self.draw.line((self.old_x, self.old_y, event.x, event.y), fill=(0,0,0,255), width=5)
+            
+       self.old_x = event.x
+       self.old_y = event.y
+        
     def save_canvas_as_image(self, folder_path, filename):
     
        image_path = os.path.realpath(folder_path + '\\' + filename)
